716a429b3cbceca5e8d1d09a9283903601152dda
Akash Agrawal <aku13693@gmail.com>
2020-10-04 20:13:19 +0530

    f

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,39 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatefulWidget {
+  @override
+  _BallPageState createState() => _BallPageState();
+}
+
+class _BallPageState extends State<BallPage> {
+  var ballPage = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.amberAccent,
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        shadowColor: Colors.black,
+        title: Text('My Decision is ......'),
+        toolbarHeight: 70,
+      ),
+      body: Center(
+        child: FlatButton(
+          onPressed: (){
+            setState(() {
+              ballPage = Random().nextInt(5)+1;
+            });
+          },
+          child: Image.asset('images/ball$ballPage.png'),
+        ),
+      ),
+    );
+  }
+}
